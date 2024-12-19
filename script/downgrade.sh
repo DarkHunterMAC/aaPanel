f2307427fb0d4222ab31295dca600587b83476c8
Aman Raz Madheshiya <105147791+DarkHunterMAC@users.noreply.github.com>
2024-12-19 15:25:53 +0530

    Update downgrade.sh

--- script/downgrade.sh
+++ script/downgrade.sh
@@ -8,18 +8,18 @@ LANG=en_US.UTF-8
 panel_path='/www/server/panel'
 
 if [ ! -d $panel_path ];then
-echo "当前未安装aapanel面板!"
+echo "aaPanel is currently not installed!"
 exit 0;
 fi
 
 base_dir=$(cd "$(dirname "$0")";pwd)
 if [ $base_dir = $panel_path ];then
-echo "不能在面板根目录执行降级命令!"
+echo "Cannot execute downgrade command in panel root directory!"
 exit 0;
 fi
 
 if [ ! -d $base_dir/class ];then
-echo "没有找到降级文件!"
+echo "No downgrade file found!"
 exit 0;
 fi
 
@@ -27,4 +27,4 @@ rm -f $panel_path/*.pyc $panel_path/class/*.pyc
 \cp -r -f $base_dir/. $panel_path/
 /etc/init.d/bt restart
 echo "===================================="
-echo "已完成降级!"
+echo "Downgrade completed!"
